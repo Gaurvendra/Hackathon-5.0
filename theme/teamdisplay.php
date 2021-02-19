5c8deb4c2cd319d5a567c20bff46a51557c75e87
Gaurvendra Singh <gaurvendra19072001@gmail.com>
2021-02-19 17:51:00 +0530

    Themes Corrected

--- theme/teamdisplay.php
+++ theme/teamdisplay.php
@@ -43,14 +43,12 @@
             <div class="container-fluid">
                 <div class="row align-items-center">
                     <div class="col-11 col-xl-2">
-                     <h1 class="mb-0"><a href="logout.php" class="text-white h2 mb-0"><img src="../images/AltRed.png" style="width: auto; height: 100%; float:left;"> </a></h1>
+                     <h1 class="mb-0"><a href="../index.html" class="text-white h2 mb-0"><img src="../images/AltRed.png" style="width: auto; height: 100%; float:left;"> </a></h1>
                     </div>
                     <div class="col-12 col-md-10 d-none d-xl-block">
                         <nav class="site-navigation position-relative text-right" role="navigation">
                             <ul class="site-menu js-clone-nav mx-auto d-none d-lg-block">
                                 <li><a href="../index.html">Home</a></li>
-                                <li><a href="../about.html">About</a></li>
-                                <li><a href="../tobe.html">Speakers</a></li>
                                 <li><a href="../tobe.html">Schedule</a></li>
                          
 
