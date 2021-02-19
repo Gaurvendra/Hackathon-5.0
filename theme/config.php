7a9b95dee50221af6f6d4a3af9ae7d6d95702559
Gaurvendra Singh <gaurvendra19072001@gmail.com>
2021-02-19 10:25:25 +0530

    Themes Corrected

--- theme/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$con = mysql_connect("localhost", "root", "3Nfx0w@1") or die("Could not connect to MySQL");
-mysql_select_db("hack",$con) or die("Could not select db");
-
-?>
--- theme/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['email']);
-if (isset($_SESSION['name'])) {
-    unset($_SESSION['name']);
-}
-session_destroy();
-header('location:../index.html');
